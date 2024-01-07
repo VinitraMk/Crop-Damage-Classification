a1491211b96d168d13f53345b79261967132e4d5
Vinitra Muralikrishnan <vinitramk@gmail.com>
2024-01-07 23:10:52 +0000

    completing fixes for basic model testing and submission build

--- dataloading/dataset.py
+++ dataloading/dataset.py
@@ -9,13 +9,14 @@ import torch
 
 class CropDataset(Dataset):
 
-    def __init__(self, label_csv_path, name2numlblmap, transforms = None):
+    def __init__(self, label_csv_path, name2numlblmap, is_test = False, transforms = None):
         config = get_config()
         self.data_dir = config['data_dir']
         self.img_dir = config['img_dir']
         self.image_labels = pd.read_csv(os.path.join(self.data_dir, label_csv_path))
         self.transforms = transforms
         self.name2numlblmap = name2numlblmap
+        self.is_test = is_test
 
     def __len__(self):
         return len(self.image_labels)
@@ -23,11 +24,14 @@ class CropDataset(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-
-        img_tensor_path = os.path.join(self.img_dir, self.image_labels.iloc[idx, 2])
+        img_tensor_path = os.path.join(self.img_dir, self.image_labels.loc[idx, 'filename'])
         img_tensor = io.imread(img_tensor_path)
-        label = self.name2numlblmap[self.image_labels.iloc[idx,1]]
-        sample = { 'id': self.image_labels.iloc[idx, 0], 'image': img_tensor, 'label': label, 'class': self.image_labels.iloc[idx, 1], 'filename': self.image_labels.iloc[idx, 2] }
+        label = self.name2numlblmap[self.image_labels.loc[idx, 'damage']] if not(self.is_test) else ''
+        sample = { 'id': self.image_labels.loc[idx, 'ID'],
+            'image': img_tensor, 'label': label,
+            'class': self.image_labels.loc[idx, 'damage'] if not(self.is_test) else '',
+            'filename': self.image_labels.loc[idx, 'filename']
+        }
 
         if self.transforms:
             sample = self.transforms(sample)
